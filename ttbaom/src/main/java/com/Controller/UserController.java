4b2c0a334365f2ca25f1e3ea2036a6705fbf4605
Tim Huo <aunm123@yeah.net>
2017-05-11 18:40:48 +0800

    no message

--- ttbaom/src/main/java/com/Controller/UserController.java
+++ ttbaom/src/main/java/com/Controller/UserController.java
@@ -1,10 +1,14 @@
 package com.Controller;
 
+import com.Entity.User;
 import com.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by timhuo on 2017/4/20.
@@ -14,8 +18,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("./user")
-    public String userList(@RequestParam(name = "callback") String callback){
+    @RequestMapping("/user")
+    public String userList(){
+        System.out.println("asdasdasdasdas");
         return "success";
+    }
+
+    @RequestMapping(value = "/userjson",method = RequestMethod.GET)
+    public @ResponseBody List<User> userListJson(){
+        return userService.findAll();
+    }
+
+    @RequestMapping(value = "/hello", produces = "text/plain;charset=UTF-8")
+    public @ResponseBody String hello() {
+        return "你好！hello";
     }
 }
