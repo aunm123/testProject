2960e27ec3e84d2f2925a660456d50f2672eb815
Tim Huo <aunm123@yeah.net>
2017-05-12 18:30:31 +0800

    8888888

--- ttbaom/src/main/java/com/Controller/UserController.java
+++ ttbaom/src/main/java/com/Controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -18,10 +19,21 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/user")
-    public String userList(){
-        System.out.println("asdasdasdasdas");
-        return "success";
+    @RequestMapping(value = "/user",method = RequestMethod.GET)   //查
+    public @ResponseBody String userListGet(@RequestParam(name = "name",required = false,defaultValue = "defaultName")String name){
+        return name+" get ";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.POST)  //增
+    public @ResponseBody String userListPost(@RequestParam(name = "name",required = false,defaultValue = "defaultName")String name){
+        return name+" post ";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.PUT)   //改
+    public @ResponseBody String userListPut(@RequestParam(name = "name",required = false,defaultValue = "defaultName")String name){
+        return name+" put ";
+    }
+    @RequestMapping(value = "/user",method = RequestMethod.DELETE) //删
+    public @ResponseBody String userListDelete(@RequestParam(name = "name",required = false,defaultValue = "defaultName")String name){
+        return name+" delete ";
     }
 
     @RequestMapping(value = "/userjson",method = RequestMethod.GET)
@@ -32,5 +44,15 @@ public class UserController {
     @RequestMapping(value = "/hello", produces = "text/plain;charset=UTF-8")
     public @ResponseBody String hello() {
         return "你好！hello";
+    }
+
+    @RequestMapping(value = "/hellojsp", method = RequestMethod.GET)
+    public String hellojsp() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String indexjsp() {
+        return "index";
     }
 }
